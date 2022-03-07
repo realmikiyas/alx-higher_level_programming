58754b952041eda791c9ee0639bf0420e2d1502b
Mikiyas Eyob <86609715+realmikiyas@users.noreply.github.com>
2022-03-07 03:18:03 -0800

    object relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.sql
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.sql
@@ -1,6 +1,6 @@
--- Create database hbtn_0e_14_usa, tables states and cities + some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_14_usa;
-USE hbtn_0e_14_usa;
+-- Create database hbtn_0e_0_usa_usa, tables states and cities + some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
 
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
