b2606126971ea1b8f7107c5823215541d9966a34
Mikiyas Eyob <86609715+realmikiyas@users.noreply.github.com>
2022-07-28 22:36:55 -0700

    newupdate

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
 const newDict = {};
-for (const key in dict) {
+
+Object.keys(dict).map(function (key, index) {
   if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+    newDict[dict[key]] = [];
   }
-}
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
